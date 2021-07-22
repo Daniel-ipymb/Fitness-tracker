68af8be664143a1763ab59af7f5cf8a677e28a5b
Daniel O <danielogunlana23@gmail.com>
2021-07-22 00:55:27 -0400

    updated api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,32 @@ const db = require("../models");
 
 
 router.post("/api/workouts", async (req,res) => {
-  const createdWorkout = await db.Workout.create(req.body)
+  const createdWorkout = await db.Workout.create({})
   console.log(createdWorkout)
   res.json(createdWorkout)
 })
 
 router.put("/api/workouts/:id", async (req,res) => {
-  const updatedWorkout = await db.Workout.findOneAndUpdate({_id: req.params.id}, {
+  const updatedWorkout = await db.Workout.findByIdAndUpdate(req.params.id, {
     $push: {
       exercises: req.body
-    }
-  })
+    },
+  },{new:true})
+  res.json(updatedWorkout)
   console.log(updatedWorkout)
 })
 
-router.get("/")
+router.get("/api/workouts/range", async (req,res) => {
+  const allWorkouts = await db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {$sum: "$exercises.duration"}
+      }
+    }
+  ]).sort({_id:-1}).limit(7)
+  console.log(allWorkouts)
+  res.json(allWorkouts)
+});
 
 router.get("/api/workouts", async (req,res) => {
   const allWorkouts = await db.Workout.aggregate([
@@ -30,7 +41,5 @@ router.get("/api/workouts", async (req,res) => {
   console.log(allWorkouts)
   res.json(allWorkouts)
 });
-
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 app.use(require("./routes/htmlRoutes"))
 app.use(require("./routes/apiRoutes"))
 
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
